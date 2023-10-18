5ec164ffd572b97e5c8dc3153d7481f95185d9c0
Bananathoms <116547351+Bananathoms@users.noreply.github.com>
2023-10-18 03:28:52 +0200

    test des operations

--- CountOnMeTests/SimpleCalcTests.swift
+++ CountOnMeTests/SimpleCalcTests.swift
@@ -10,21 +10,41 @@ import XCTest
 @testable import CountOnMe
 
 class SimpleCalcTests: XCTestCase {
-    var calculatorModel: CalculatorModel!
+    var calculator: CalculatorModel!
     
     override func setUp() {
         super.setUp()
-        calculatorModel = CalculatorModel()
+        calculator = CalculatorModel()
     }
 
     override func tearDown() {
-        calculatorModel = nil
+        calculator = nil
         super.tearDown()
     }
 
     func testAddition() {
-        let result = calculatorModel.add(3.0, 4.0)
+        let result = calculator.add(3.0, 4.0)
         XCTAssertEqual(result, 7.0, "L'addition de 3.0 et 4.0 devrait être égale à 7.0")
+    }
+    
+    func testSubtraction() {
+        let result = calculator.subtract(10.0, 4.0)
+        XCTAssertEqual(result, 6.0, "La soustraction a échoué")
+    }
+    
+    func testMultiplication() {
+        let result = calculator.multiply(7.0, 3.0)
+        XCTAssertEqual(result, 21.0, "La multiplication a échoué")
+    }
+    
+    func testDivision() {
+        let result = calculator.divide(20.0, 4.0)
+        XCTAssertEqual(result, 5.0, "La division a échoué")
+    }
+    
+    func testDivisionByZero() {
+        let result = calculator.divide(10.0, 0.0)
+        XCTAssertTrue(result.isNaN, "La division par zéro n'a pas renvoyé NaN")
     }
 
 }
