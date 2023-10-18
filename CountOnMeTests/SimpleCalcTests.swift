2f8fb91f7286a53dab9b2fb7f6dc5fbdbd18bb45
Bananathoms <116547351+Bananathoms@users.noreply.github.com>
2023-10-18 03:24:24 +0200

    Debut mise en place test

--- CountOnMeTests/SimpleCalcTests.swift
+++ CountOnMeTests/SimpleCalcTests.swift
@@ -7,28 +7,24 @@
 //
 
 import XCTest
-@testable import SimpleCalc
+@testable import CountOnMe
 
 class SimpleCalcTests: XCTestCase {
-
+    var calculatorModel: CalculatorModel!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        calculatorModel = CalculatorModel()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        calculatorModel = nil
+        super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testAddition() {
+        let result = calculatorModel.add(3.0, 4.0)
+        XCTAssertEqual(result, 7.0, "L'addition de 3.0 et 4.0 devrait être égale à 7.0")
     }
 
 }
